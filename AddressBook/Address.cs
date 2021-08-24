cdaf79489e234554afaaabdd7f552a533a318670
Sagar Lad <sagar.lad@morningstar.com>
2021-08-24 23:37:16 +0530

    Added Address Book and imported data to csv

---
+++ AddressBook/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Address
+    {
+        public string name;
+        public string address;
+
+        public Address(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+    }
+}
---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        List<Address> addresses;
+
+        public AddressBook()
+        {
+            addresses = new List<Address>();
+        }
+
+        public bool add(string name, string address)
+        {
+            Address addr = new Address(name, address);
+            Address result = find(name);
+
+            if (result == null)
+            {
+                addresses.Add(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool remove(string name)
+        {
+            Address addr = find(name);
+
+            if (addr != null)
+            {
+                addresses.Remove(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void list(Action<Address> action)
+        {
+            addresses.ForEach(action);
+        }
+
+        public bool isEmpty()
+        {
+            return (addresses.Count == 0);
+        }
+
+        public Address find(string name)
+        {
+            Address addr = addresses.Find((a) => a.name == name);
+            return addr;
+        }
+    }
+}
